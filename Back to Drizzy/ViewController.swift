cc31ef401cdaa85242cb3feff545f9a61d512232
adityamaru27 <a2maru@uwaterloo.ca>
2016-05-02 14:20:50 -0400

    Designed an Audio Player which plays a pre downloaded song. Allows scrubbing and volume control via sliders

--- Back to Drizzy/ViewController.swift
+++ Back to Drizzy/ViewController.swift
@@ -7,17 +7,87 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player:AVAudioPlayer = AVAudioPlayer()
 
-    override func viewDidLoad() {
+    @IBAction func adjustVolume(sender: AnyObject)
+    {
+        player.volume = (volumeSlider?.value)!
+        
+    }
+    
+
+    @IBAction func adSc(sender: AnyObject)
+    {
+        player.currentTime = NSTimeInterval(scrubberSlider.value)
+    }
+    
+    @IBOutlet weak var volumeSlider: UISlider?
+    
+    @IBOutlet weak var scrubberSlider: UISlider!
+    
+    @IBAction func Play(sender: AnyObject)
+    {
+        player.play()
+        
+    }
+    
+    
+    @IBAction func Pause(sender: AnyObject)
+    {
+        player.pause()
+        
+    }
+    
+    @IBAction func Stop(sender: AnyObject)
+    {
+        player.pause()
+        do
+        {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Trophies", ofType: "mp3")!))
+        
+        }
+        catch{
+            //it didnt work!
+        }
+        
+        
+    }
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do
+        {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Trophies", ofType: "mp3")!))
+            
+            scrubberSlider.maximumValue = Float(player.duration)
+        }
+        catch{
+            //it didnt work!
+        }
+        
+        _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.adScU), userInfo: nil, repeats: true)
+        
+        
+        
+    }
+    
+    func adScU()
+    {
+        scrubberSlider.value = Float(player.currentTime)
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
